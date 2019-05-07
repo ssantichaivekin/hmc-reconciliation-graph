449b618601f77189172eb53b2fa1dece1e700ee1
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2019-05-06 17:23:53 -0700

    remove coverage log

--- trace_clean_up.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    if ',' in line:
-        fileSection, moduleSection, funcSection = line.split(',')
-        funcSection = funcSection.strip()
-        funcName = funcSection.split()[1]
-        print(funcName)
